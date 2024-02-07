d456ac3c48a0044971e3519626413b6624a890ee
Neil Cedric Sapno <104262695+neilsapno@users.noreply.github.com>
2024-02-08 00:45:31 +0800

    Update ModuleInfo.java

--- app/src/main/java/tn/amin/mpro2/constants/ModuleInfo.java
+++ app/src/main/java/tn/amin/mpro2/constants/ModuleInfo.java
@@ -7,8 +7,8 @@ public class ModuleInfo {
     public static final String LINK_GITHUB_WIKI_USAGE_GUIDE = "https://github.com/Mino260806/MessengerPro/wiki/Usage-Guide/";
 
     public static final int MIN_ORCA_VERSION = 317807898;
-    public static final int MAX_ORCA_VERSION = 317807978;
-    public static final String RECOMMENDED_ORCA_VERSION_STRING = "439.0.0.29.119";
+    public static final int MAX_ORCA_VERSION = 319001817;
+    public static final String RECOMMENDED_ORCA_VERSION_STRING = "445.0.0.0.39";
 
     public static final String PACKAGE_AI_PLUGIN = "tn.amin.mproai";
 }
